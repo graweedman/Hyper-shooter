10483fafff3ab0a5aad5759d3e93d83046552616
Graweed <niks@precisionlex.com>
2023-12-15 12:36:45 +0200

    Commit

--- player.gd
+++ player.gd
@@ -1,14 +1,16 @@
 extends CharacterBody3D
 
 @onready var neck = $neck
-@onready var head = $neck/head
-@onready var camera = $neck/head/camera
-@onready var grapple_raycast = $neck/head/camera/grapple_cast
+@onready var head = $neck/head 
+@onready var eyes = $neck/head/eyes
+@onready var camera = $neck/head/eyes/camera
+@onready var grapple_raycast = $neck/head/eyes/camera/grapple_cast
 @onready var standing_collision_shape = $standing_collision_shape
 @onready var crouching_collision_shape = $crouching_collision_shape
 @onready var sliding_collision_shape = $sliding_collision_shape
 @onready var character_raycast = $player_cast
 @onready var head_bonk_raycast = $head_bonk
+
 
 # exported variables
 @export var WALKING_SPEED = 5.0
@@ -19,6 +21,7 @@ extends CharacterBody3D
 
 const JUMP_VELOCITY = 4.5
 const LERP_SPEED = 10.0
+const AIR_LERP_SPEED = 5.0
 const MOUSE_SENSITIVITY = 0.4
 
 # movement variables
@@ -36,7 +39,13 @@ const head_bobbing_sprinting_speed = 22.0
 const head_bobbing_walking_speed = 14.0
 const head_bobbing_crouching_speed = 10.0
 
-var head_bobbing_speed = 0.0
+const head_bobbing_crouching_intensity = 0.2
+const head_bobbing_walking_intensity = 0.1
+const head_bobbing_sprinting_intensity = 0.05
+
+var head_bobbing_vector = Vector2.ZERO
+var head_bobbing_index = 0.0
+var head_bobbing_current_intensity = 0.0
 
 # states
 var grappling = false
@@ -76,6 +85,8 @@ func _physics_process(delta):
 
 	grapple()
 
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+
 	if sliding:
 		slide_timer -= delta
 		if slide_timer <= 0:
@@ -83,13 +94,37 @@ func _physics_process(delta):
 			sliding = false
 			print("free_look_stop", free_look)
 
+	# Head bobbing.
+	if running:
+		head_bobbing_current_intensity = head_bobbing_sprinting_intensity
+		head_bobbing_index += head_bobbing_sprinting_speed * delta
+	elif walking:
+		head_bobbing_current_intensity = head_bobbing_walking_intensity
+		head_bobbing_index += head_bobbing_walking_speed * delta
+	elif crouching:
+		head_bobbing_current_intensity = head_bobbing_crouching_intensity
+		head_bobbing_index += head_bobbing_crouching_speed * delta
+	else:
+		head_bobbing_current_intensity = 0.0
+
+	if is_on_floor() && !sliding && input_dir != Vector2.ZERO:
+		head_bobbing_vector.y = sin(head_bobbing_index)
+		head_bobbing_vector.x = cos(head_bobbing_index/ 2)+0.5
+
+		eyes.position.y = lerp(eyes.position.y, head_bobbing_vector.y * (head_bobbing_current_intensity/2.0), delta*LERP_SPEED)
+		eyes.position.x = lerp(eyes.position.x, head_bobbing_vector.x * (head_bobbing_current_intensity), delta*LERP_SPEED)
+
+	else:
+		eyes.position.y = lerp(eyes.position.y, 0.0, delta*LERP_SPEED)
+		eyes.position.x = lerp(eyes.position.x, 0.0, delta*LERP_SPEED)
+		head_bobbing_vector = Vector2.ZERO
+
 	if not is_on_floor():
 		if not grappling:
 			velocity.y -= gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") && is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	
+	
 
 	# Handle Free Look.
 	if Input.is_action_pressed("free_look") || sliding:
@@ -100,58 +135,58 @@ func _physics_process(delta):
 		free_look = false
 		neck.rotation.y = lerp(neck.rotation.y, 0.0, delta*LERP_SPEED)
 		camera.rotation.z = lerp(camera.rotation.z, 0.0, delta*LERP_SPEED)
+
+	if Input.is_action_pressed("crouch") || sliding:
 		
-	# Handle input.
-	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	
 
-	# Crouching/sliding.
-	
-	if Input.is_action_pressed("sprint") && !crouching:
-		player_state("running")
+		standing_collision_shape.disabled = true
+		crouching_collision_shape.disabled = false
 
-	if Input.is_action_pressed("crouch"):
-		grappling = false
-		current_speed = CROUCHING_SPEED
+		current_speed = lerp(current_speed, CROUCHING_SPEED, delta*LERP_SPEED)
+		head.position.y = lerp(head.position.y, crouching_depth, delta*LERP_SPEED)
 
-		if running:
-			print("sliding")
+		if running && input_dir != Vector2.ZERO:
 			sliding = true
 			slide_timer = SLIDE_TIMER_MAX
 			slide_vector = input_dir
-			print("free_look_start", free_look)
+
 		player_state("crouching")
-		head.position.y = lerp(head.position.y, crouching_depth, delta*LERP_SPEED)
-		standing_collision_shape.disabled = true
-		crouching_collision_shape.disabled = false
+		
 	elif !character_raycast.is_colliding() && !sliding:
-		if running:
-			current_speed = RUNNING_SPEED
-			player_state("running")
-		else:
-			current_speed = WALKING_SPEED
-			player_state("walking")
+
 		standing_collision_shape.disabled = false
 		crouching_collision_shape.disabled = true
 		head.position.y = lerp(head.position.y, 0.0, delta*LERP_SPEED)
-		
-	
 
-	direction = lerp(direction, (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(), delta*LERP_SPEED)
+		if Input.is_action_pressed("sprint"):
+			player_state("running")
+			current_speed = lerp(current_speed, RUNNING_SPEED, delta*LERP_SPEED)
+		else:
+			player_state("walking")
+			current_speed = lerp(current_speed, WALKING_SPEED, delta*LERP_SPEED)
+
+	# Handle Airtime
+	if is_on_floor():
+		direction = lerp(direction, (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(), delta*LERP_SPEED)
+	else:
+		if input_dir != Vector2.ZERO:
+			direction = lerp(direction, (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(), delta*AIR_LERP_SPEED)
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") && is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		sliding = false
 
 	if sliding:
 		direction = (transform.basis * Vector3(slide_vector.x, 0, slide_vector.y)).normalized()
+		current_speed = (slide_timer + 0.1) * SLIDING_SPEED
 	
 	if direction:
 		velocity.x = direction.x * current_speed
 		velocity.z = direction.z * current_speed
-
-		if sliding:
-			velocity.x = direction.x * (slide_timer + 0.1) * RUNNING_SPEED
-			velocity.z = direction.z * (slide_timer + 0.1) * RUNNING_SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, current_speed)
-		velocity.z = move_toward(velocity.z, 0, current_speed)
+	# else:
+		# velocity.x = move_toward(velocity.x, 0, current_speed)
+		# velocity.z = move_toward(velocity.z, 0, current_speed)
 	move_and_slide()
 
 
